5d24c776f0111cd3968b3a002ce86b76934f0602
Nignesh <NiGi@Nigneshs-MacBook-Pro.local>
2014-12-15 23:44:26 -0500

    MISC

--- js/index.js
+++ js/index.js
@@ -13,47 +13,47 @@ var rightEar
 $(document).ready(function () {
 
 	
-	loadSVGInlineH();
-	loadSVGInlineM();
+//	loadSVGInlineH();
+//	loadSVGInlineM();
 });
 
-function loadSVGInlineH()
-{
-	
-    var SVGFile="headphone.svg"
-    var loadXML = new XMLHttpRequest;
-    function handler(){
-        if(loadXML.readyState == 4 && loadXML.status == 200)
-        {
-			alert("loadSVGInlineH");
-            $("body").innerHTML=loadXML.responseText
-        }
-    }
-    if (loadXML != null){
-        loadXML.open("GET", SVGFile, true);
-        loadXML.onreadystatechange = handler;
-        loadXML.send();
-    }
-}
-
-function loadSVGInlineM()
-{
-	
-    var SVGFile="microphone.svg"
-    var loadXML = new XMLHttpRequest;
-    function handler(){
-        if(loadXML.readyState == 4 && loadXML.status == 200)
-        {
-			alert("loadSVGInlineM");
-            $("body").innerHTML=loadXML.responseText
-        }
-    }
-    if (loadXML != null){
-        loadXML.open("GET", SVGFile, true);
-        loadXML.onreadystatechange = handler;
-        loadXML.send();
-    }
-}
+//function loadSVGInlineH()
+//{
+//	
+//    var SVGFile="headphone.svg"
+//    var loadXML = new XMLHttpRequest;
+//    function handler(){
+//        if(loadXML.readyState == 4 && loadXML.status == 200)
+//        {
+//			alert("loadSVGInlineH");
+//            $("body").innerHTML=loadXML.responseText
+//        }
+//    }
+//    if (loadXML != null){
+//        loadXML.open("GET", SVGFile, true);
+//        loadXML.onreadystatechange = handler;
+//        loadXML.send();
+//    }
+//}
+//
+//function loadSVGInlineM()
+//{
+//	
+//    var SVGFile="microphone.svg"
+//    var loadXML = new XMLHttpRequest;
+//    function handler(){
+//        if(loadXML.readyState == 4 && loadXML.status == 200)
+//        {
+//			alert("loadSVGInlineM");
+//            $("body").innerHTML=loadXML.responseText
+//        }
+//    }
+//    if (loadXML != null){
+//        loadXML.open("GET", SVGFile, true);
+//        loadXML.onreadystatechange = handler;
+//        loadXML.send();
+//    }
+//}
 
 
 
